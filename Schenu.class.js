a1fb181bf947274f5f311c055077a1745cde2ee6
Fr13dr1ch <friedrichlucas.99@gmail.com>
2021-02-10 03:49:13 +0100

    Implement templating engine add support for: variables and extensions

--- Schenu.class.js
+++ Schenu.class.js
@@ -1,10 +1,12 @@
 /**
  * Schenu - Is a javascript based template engine.
  */
-class Schenu {
+export class Schenu {
   constructor(server) {
     this.server = server;
     this.server.Schenu = this;
+    this.parseTimeout = 100;
+    this.extensions = {};
   }
 
   /**
@@ -15,7 +17,62 @@ class Schenu {
    *
    * @returns {String} The parsed template
    */
-  parse(text, ...args) {
-    return text;
+  async parse(text, args = {}, errorBreak = false) {
+    return new Promise((resolve, error) => {
+      let tmpFileName = "./tmpSchenuWorker" + Math.round(Math.random() * 1000) + ".js";
+      let regexText = new RegExp(/\#\#([^#]*)\#\#/, "g");
+      let regexVars = new RegExp(/\$([^$]*)\$/, "g");
+      let regexFunctions = new RegExp(/\~([^(]*)\(([^)]*)\)/, "g");
+      let replaceText = text;
+
+      /**
+       * Replace template variables
+       */
+      replaceText = replaceText.replaceAll(regexVars, function(match, variable) {
+        let replacement = "' + " + variable + " + '";
+        if(args[variable] != null) {
+          replacement = args[variable];
+        }
+        return replacement;
+      });
+
+      /**
+       * Support extension functions
+       */
+      replaceText = replaceText.replaceAll(regexFunctions, function(match, name, args) {
+        if(this.extensions[name] != null) {
+          let extendedString = this.extensions[name](args);
+          return extendedString;
+        } else {
+          return match;
+        }
+      }.bind(this));
+
+      replaceText = replaceText.replaceAll(regexText, "endText = endText + '$1'");
+
+      replaceText = "let endText = '';" + replaceText + ";postMessage(endText);";
+      let encoder = new TextEncoder();
+      Deno.writeFileSync(tmpFileName, encoder.encode(replaceText));
+
+
+      let worker = new Worker(new URL(tmpFileName, import.meta.url).href, { type: "module" });
+      let timeoutId = setTimeout(() => {
+        worker.terminate();
+        Deno.removeSync(tmpFileName);
+        if(errorBreak) {
+          error("Worker timed out!");
+        } else {
+          resolve("");
+        }
+      }, this.parseTimeout);
+
+      worker.onmessage = ({data}) => {
+        clearTimeout(timeoutId);
+        let parsedText = data;
+        worker.terminate();
+        Deno.removeSync(tmpFileName);
+        resolve(parsedText);
+      }
+    });
   }
 }
